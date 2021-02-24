576d1a5148d300f32d525ec20ee96ce1d7cf6053
Weston Woodard <westonwoodard28@gmail.com>
2021-02-24 15:47:41 -0700

    form complete, next is Validation and POST

--- user-onboarding/src/App.js
+++ user-onboarding/src/App.js
@@ -1,11 +1,53 @@
 import React, {useState, useEffect} from 'react';
 import Form from './Form'
+import UserCards from './UserCards'
 import './App.css';
 
+const initialFormValues = {
+  name: '',
+  email: '',
+  password: '',
+  ToS: false,
+}
+
+
 function App() {
+
+  const [users, setUsers] = useState([])
+  const [formValues, setFormValues] = useState(initialFormValues)
+
+  const updateForm = (inputName, inputValue) => {
+    setFormValues({
+      ...formValues,
+      [inputName]: inputValue
+    })
+  }
+
+  const submitForm = () => {
+    const newUser = {
+      name: formValues.name.trim(),
+      email: formValues.email.trim(),
+      password: formValues.password.trim(),
+    }
+    if (!newUser.name || !newUser.email || !newUser.password) {
+      return;
+    }
+    setUsers([...users, newUser])
+    setFormValues(initialFormValues)
+  }
+
   return (
     <div className="App">
-      <Form />
+      <Form 
+      values={formValues}
+      update={updateForm}
+      submit={submitForm}
+      />
+      {users.map(user => {
+        return (
+          <UserCards key={user.id} user={user} />
+        )
+      })}
     </div>
   );
 }
--- user-onboarding/src/Form.js
+++ user-onboarding/src/Form.js
@@ -1,5 +1,70 @@
 import React from 'react'
 
 export default function Form(props) {
-    
+    const { values, update, submit } = props
+
+    const onChange = evt => {
+        const {name, value, type, checked } = evt. target
+        const valueToUse = type === 'checkbox' ? checked : value
+        update(name, valueToUse)
+    }
+
+
+
+const onSubmit = evt => {
+    evt.preventDefault()
+    submit()
+}
+
+return (
+    <div className="Form Container">
+        <form onSubmit={onSubmit}>
+            <label>
+                Name:
+                <input
+                    type="text"
+                    name="name"
+                    value={values.name}
+                    placeholder="enter your name here"
+                    maxLength="40"
+                    onChange={onChange}
+                />
+            </label>
+            <label>
+                Email:
+                <input
+                    type='email'
+                    name='email'
+                    value={values.email}
+                    placeholder='enter your email here'
+                    maxLength='40'
+                    onChange={onChange}
+                />
+            </label>
+            <label>
+                Password:
+                <input
+                    type="text"
+                    name="password"
+                    value={values.password}
+                    placeholder="enter your password here"
+                    maxLength="40"
+                    onChange={onChange}
+                />
+            </label>
+            <label>
+                Terms of Service
+                <input
+                    type="checkbox"
+                    name="ToS"
+                    checked={values.ToS}
+                    onChange={onChange}
+                />
+            </label>
+            <div className="submit button">
+                <button disabled={!values.name || !values.email || !values.password || !values.ToS}>submit</button>
+            </div>
+        </form>
+    </div>
+)
 }
